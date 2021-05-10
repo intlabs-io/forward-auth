36f6fdc5c51dddf0e7dd504db6f9a8ec44ec7c0e
Rick Morrison <mail@roderickmorrison.net>
2021-05-10 11:01:05 -0700

    test JWT parsing with secret key and RSA public key

--- authorizer_test.go
+++ authorizer_test.go
@@ -5,12 +5,18 @@ import (
 	"testing"
 
 	"bitbucket.org/_metalogic_/log"
-	jwt "github.com/dgrijalva/jwt-go"
+)
+
+const (
+	jwtHeader = "test"
 )
 
 var (
+	auth      *Auth
 	publicKey []byte
-	secret    = []byte("RyxDWzqg8AaDAHxGt989tEPdfG42dr6e5QqCxJ4mwGKYavtLbj")
+	blocks    map[string]bool
+	tokens    map[string]string
+	secret    []byte
 	adamTkn   = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjRFQjcxODhFMzhGOTgyNEY2M0QyQTRFQzdEMjNEMjAxREYyRTZBMjFSUzI1NiIsInR5cCI6ImF0K2p3dCIsIng1dCI6IlRyY1lqamo1Z2s5ajBxVHNmU1BTQWQ4dWFpRSJ9.eyJuYmYiOjE2MjAyNTQ4NzAsImV4cCI6MTYyMjg0Njg3MCwiaXNzIjoiaHR0cHM6Ly9sb2NhbGhvc3Q6ODA4MSIsImNsaWVudF9pZCI6Im1jIiwic3ViIjoiQ0YxQjczMDYtOUM4Qy00REZGLUE2NzgtMTBDREJDMEYzRDMxIiwiYXV0aF90aW1lIjoxNjIwMjU0NTU2LCJpZHAiOiJsb2NhbCIsImlkZW50aXR5Ijoie1widXNlckdVSURcIjpcIkNGMUI3MzA2LTlDOEMtNERGRi1BNjc4LTEwQ0RCQzBGM0QzMVwiLFwidXNlcm5hbWVcIjpcImFkYW0uYnJvd25AZWR1Y2F0aW9ucGxhbm5lcmJjLmNhXCIsXCJyb290XCI6dHJ1ZSxcInVzZXJQZXJtc1wiOlt7XCJ0ZW5hbnRJRFwiOlwiMTk2RUUzNjMtQTVBMy00QzQ3LThGREItMkU4REE4REJFMkU5XCIsXCJwZXJtc1wiOlt7XCJjYXRlZ29yeVwiOlwiQU5ZXCIsXCJhY3Rpb25zXCI6W1wiQUxMXCJdfV19LHtcInRlbmFudElEXCI6XCIyRDAzRDY3Ny02RDY0LTRGMzYtQjA5OC05Q0E0ODdFM0I2RUFcIixcInBlcm1zXCI6W3tcImNhdGVnb3J5XCI6XCJBRE1cIixcImFjdGlvbnNcIjpbXCJSRUFEXCJdfV19LHtcInRlbmFudElEXCI6XCI0NDEwNTBDMS04ODM5LTRBRTktOTY5My03OTVFNEU0RkE4NzVcIixcInBlcm1zXCI6W119LHtcInRlbmFudElEXCI6XCI1MzlFMjQ3NS0wMjE1LTQ0QzMtQTREMC1FQTgwNkNGOUFCOUZcIixcInBlcm1zXCI6W3tcImNhdGVnb3J5XCI6XCJBTllcIixcImFjdGlvbnNcIjpbXCJBTExcIl19XX1dfSIsImp0aSI6IjM3RkE1RjIzRUIzRjJEOEVGMTUyNEExMEEzNjNFMDQ0Iiwic2lkIjoiNzUyNEI0QTkxRkUxNEMzRDI5MTlDMzVFRDgzQzk0NzUiLCJpYXQiOjE2MjAyNTQ4NzAsInNjb3BlIjpbIm9wZW5pZCIsInByb2ZpbGUiLCJlbWFpbCIsImVwYmNfaWRlbnRpdHkiLCJvZmZsaW5lX2FjY2VzcyJdLCJhbXIiOlsicHdkIl19.QhGIVAT8Eb8GzNSykLJbj5SRypbpZeDgA5Gr0kja_sRRTEeOE9rLcdwKF853WFdqz4MzE-cdhndqoLjpxPyKhnM-VGJmjSPOlffYX8zJSEM3jPhzqxdHBr0d-g6lAJNr77PNq4_5CVfbo6BQ1NHQyjhxjNEmdnRiRnUNSjj2-4tBQQu5vyaKccKTm51n_eHetcNdoC5CeFlKwAJEs9zEsGSdGPmswtMVM7FEpd8B0TMFyrKihc9FJrN09Yoq3J6FSU1W7bPPkNWYU1irXNUGSTfkM2Wfx_PZcalk6XeQOsVBt0fj4ogSywyPiUXrc3RkfT8MbqQCpGuDNln8Dh6JO9HXWByj_gvPk4c_7N4TMcshw6EoGQjO42KADXGEmonAMZnnqPUioRpPJ4ai8gt2MpUIH-ZydRbcYE-hA1qktyJuqRetS8YwMN52dcS7emirX19-J8rbfJv7d7kOh17COSgI2zGV2d9UIl62YynDiKdJnyiGEgfxiKHhr8JfEqypVUSRWnplbqUTUOwAEDKxgHthXG8McgbHWe86QFTxrVQIXWUnaCDllw6sh8EUtMVQ0ElnEoliSVH4Buorr5-qg4cFEA-41Tcw4M-DieqO9JXxxaiV178_6MJR69VPLmKZKZHyCqTUg729THiCHlk8eka5RBuFBEx1KWR0S0MJFbU"
 	adamJWT   = `{
 		"nbf": 1620162215,
@@ -36,7 +42,7 @@ var (
 		]
 	  }`
 	// use auth-api/v1/login to get rickyTkn string
-	rickyTkn = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZGVudGl0eSI6eyJ1c2VyR1VJRCI6IjYwMENBOTI3LTVFMkItNDhCNC04Qzk1LUE1Qjg4MzlDM0E3RiIsInVzZXJuYW1lIjoicmlja3lAZWR1Y2F0aW9ucGxhbm5lcmJjLmNhIiwicm9vdCI6dHJ1ZSwidXNlclBlcm1zIjpbeyJ0ZW5hbnRJRCI6IjQ0MTA1MEMxLTg4MzktNEFFOS05NjkzLTc5NUU0RTRGQTg3NSIsInBlcm1zIjpbeyJjYXRlZ29yeSI6IlBST0ciLCJhY3Rpb25zIjpbIkNSRUFURSIsIkRFTEVURSIsIlJFQUQiLCJVUERBVEUiXX1dfV19LCJleHAiOjE2MjAyNjIxOTksImlhdCI6MTYyMDI2MDM5OSwiaXNzIjoiRWR1Y2F0aW9uUGxhbm5lckJDIn0.tcQ7kaWTNomeqtrw7rstO64xyGhWHR4BOS-v58LwlgA"
+	rickyTkn = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZGVudGl0eSI6eyJ1c2VyR1VJRCI6IjYwMENBOTI3LTVFMkItNDhCNC04Qzk1LUE1Qjg4MzlDM0E3RiIsInVzZXJuYW1lIjoicmlja3lAZWR1Y2F0aW9ucGxhbm5lcmJjLmNhIiwicm9vdCI6dHJ1ZSwidXNlclBlcm1zIjpbeyJ0ZW5hbnRJRCI6IjQ0MTA1MEMxLTg4MzktNEFFOS05NjkzLTc5NUU0RTRGQTg3NSIsInBlcm1zIjpbeyJjYXRlZ29yeSI6IlBST0ciLCJhY3Rpb25zIjpbIkNSRUFURSIsIkRFTEVURSIsIlJFQUQiLCJVUERBVEUiXX1dfV19LCJleHAiOjE2MjA0MjYxODcsImlhdCI6MTYyMDQyNDM4NywiaXNzIjoiRWR1Y2F0aW9uUGxhbm5lckJDIn0.QuCnG3IQKZRdVfdsgjuQezbtV4dW_aj9Cj2OHDk_smc"
 	rickyJWT = `{
 		"nbf": 1619721524,
 		"exp": 1619725124,
@@ -84,6 +90,11 @@ var (
 func init() {
 	var err error
 	publicKey, err = ioutil.ReadFile("test/public.key")
+	if err != nil {
+		log.Fatal(err)
+	}
+	secret = []byte("RyxDWzqg8AaDAHxGt989tEPdfG42dr6e5QqCxJ4mwGKYavtLbj")
+	auth, err = NewAuth(jwtHeader, publicKey, secret, tokens, blocks)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -141,34 +152,19 @@ func Test_checkJWT(t *testing.T) {
 	}
 
 	for _, tt := range tests {
-		switch tt.args.keyType {
-		case "secret":
-			t.Run(tt.name, func(t *testing.T) {
-				identity, err := checkSecret(tt.args.key, tt.args.tokenString)
-				if err != nil {
-					t.Errorf("%s: %s", tt.name, err)
-				}
+		t.Run(tt.name, func(t *testing.T) {
+			identity, err := auth.JWTIdentity(tt.args.tokenString)
+			if err == nil {
 				if identity.Root != tt.wants.root {
 					t.Errorf("%s: identity.Root = %t, want %t", tt.name, identity.Root, tt.wants.root)
 				}
-			})
-		case "rsa":
-			t.Run(tt.name, func(t *testing.T) {
-				rsaKey, err := jwt.ParseRSAPublicKeyFromPEM(tt.args.key)
-				if err != nil {
-					t.Error(err)
-				}
-				identity, err := checkRSA(rsaKey, tt.args.tokenString)
-				if err != nil {
-					t.Errorf("%s: %s", tt.name, err)
-				}
-				if identity.Root != tt.wants.root {
-					t.Errorf("%s: identity.Root = %t, want %t", tt.name, identity.Root, tt.wants.root)
-				}
-				if identity.Username != "adam.brown@educationplannerbc.ca" {
+				if identity.Username != tt.wants.username {
 					t.Errorf("%s: identity.Username = %s, want %s", tt.name, identity.Username, tt.wants.username)
 				}
-			})
-		}
+			} else {
+				t.Errorf("%s: %s", tt.name, err)
+			}
+
+		})
 	}
 }
