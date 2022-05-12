f3f872574ef14d5023cf130cabbb958e5b0d9a74
Ricky Morrison <ricky@educationplannerbc.ca>
2022-05-12 13:39:25 -0700

    implement Reload(acs) as TODO

--- stores/mssql/mssql.go
+++ stores/mssql/mssql.go
@@ -118,6 +118,12 @@ func (store *MSSql) Load() (acs *fauth.AccessSystem, err error) {
 	return acs, nil
 }
 
+// Reload updates acs from files
+func (store *MSSql) Reload(acs *fauth.AccessSystem) (err error) {
+	// TODO
+	return nil
+}
+
 // Blocks returns the map of blocked users
 // TODO this needs to come from the database
 func (store *MSSql) Blocks() (map[string]bool, error) {
