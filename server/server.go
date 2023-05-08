8897d9d4f0c318c9487324abd73462fdcbf30878
Ricky Morrison <dev@roderickmorrison.net>
2023-05-08 14:21:46 +0000

    additional CORS headers

--- server/server.go
+++ server/server.go
@@ -127,6 +127,8 @@ func router(auth *fauth.Auth, store fauth.Store, userHeader, traceHeader string)
 	corsFunc := func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			w.Header().Set("Access-Control-Allow-Origin", "*")
+			w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+			w.Header().Set("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, HEAD, OPTIONS")
 			next.ServeHTTP(w, r)
 		})
 	}
