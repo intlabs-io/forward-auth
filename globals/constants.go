9664ab480c399ed6d69c68cfce1e77d3f493d7bd
Ricky Morrison <dev@roderickmorrison.net>
2023-08-02 17:58:40 +0000

    channel type constants

---
+++ globals/constants.go
@@ -0,0 +1,13 @@
+// package globals defines global constants and variables dot-imported throughout the application
+
+package globals
+
+const (
+	// these should agree with base.CONTACT_CHANNELS
+	CHANNEL_EMAIL = "EMAIL"
+	CHANNEL_SLACK = "SLACK"
+
+	// not yet supported
+	CHANNEL_SMS      = "SMS"
+	CHANNEL_WHATSAPP = "WHATSAPP"
+)
